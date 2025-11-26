74ee6e7b86f48350a4d077ae5ef27b7cc4f805e4
Pynk Fysh <github.elongated500@passmail.com>
2025-11-26 15:59:24 -0800

    added firebase, updated calendar with festival popup

--- app/index.tsx
+++ app/index.tsx
@@ -2,26 +2,23 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Calendar } from 'react-native-calendars';
 
-const EVENTDATA = [
-  {
-    title: '2025-10-01',
-    data: [
-      {hour: '12am', duration: '1h', title: 'Holi'},
-    ],
-  },
-  {
-    title: '2025-10-15',
-    data: [
-      {hour: '9am', duration: '1h', title: 'Republic Day'}
-    ],
-  },
-  {
-    title: '2025-10-20',
-    data: [
-      {hour: '9am', duration: '1h', title: 'Diwali'}
-    ],
-  },
-];
+/*
+TO DO
+
+Add Navigation
+https://reactnative.dev/docs/navigation
+1. Update festival card component to add "More Info..." link
+2. Route to more info, and render information about that specific festival
+
+Organize the code
+https://dev.to/paulocappa/how-to-organize-your-components-in-react-native-folder-structure-and-project-organization-1hke
+
+Gregorian Cal Date to panchāngam Hindu Cal Date Conversion
+https://en.wikipedia.org/wiki/Panchangam
+https://github.com/schenna/panchangJS
+
+Set up Database
+*/
 
 const styles = StyleSheet.create({
   center: {
@@ -34,72 +31,117 @@ const styles = StyleSheet.create({
   },
 });
 
-const HinduCalendar = () => {
-  const [selected, setSelected] = useState('');
-
-  return (
-    <View>
-      <Calendar
-        onDayPress={day => {
-          setSelected(day.dateString);
-        }}
-        markedDates={{
-          '2025-10-20': {marked: true, dotColor: '#f3ca13ff'},
-          [selected]: {selected: true, disableTouchEvent: true, selectedColor: '#f3ca13ff'}
-        }}
-        theme={{
-          backgroundColor: '#ffffff', // Overall background color
-          calendarBackground: '#ffffff', // Calendar background color
-          textSectionTitleColor: '#b6c1cd', // Color for section titles (weekdays)
-          selectedDayBackgroundColor: '#00adf5', // Background color for selected day
-          selectedDayTextColor: '#ffffff', // Text color for selected day
-          todayTextColor: '#f3ca13ff', // Text color for today's date
-          dayTextColor: '#2d4150', // Default day text color
-          textDisabledColor: '#d9e1e8', // Color for disabled days
-          dotColor: '#f3ca13ff', // Default dot color
-          selectedDotColor: '#f3ca13ff', // Dot color for selected day
-          arrowColor: '#f3ca13ff', // Color for navigation arrows
-          monthTextColor: '#f3ca13ff', // Color for month text
-          indicatorColor: 'yellow', // Color for loading indicator
-          textDayFontFamily: 'monospace', // Font family for day numbers
-          textMonthFontFamily: 'monospace', // Font family for month text
-          textDayHeaderFontFamily: 'monospace', // Font family for day headers
-          textDayFontSize: 16, // Font size for day numbers
-          textMonthFontSize: 16, // Font size for month text
-          textDayHeaderFontSize: 16 // Font size for day headers
-        }}
-      />
-    </View>
-  );
-};
+type FestivalItem = {
+  date: string; //YY-MM-DD
+  name: string;
+  description: string;
+}
 
 type FestivalProps = {
   name: string;
 };
 
-const Festival = (props: FestivalProps) => {
-  return (
-    <View style={styles.center}>
-      <Text>Today is {props.name}</Text>
-    </View>
-  )
-}
+const EVENTDATA: FestivalItem[] = [
+  {
+    date: '2025-10-01',
+    name: 'Holi', 
+    description: 'The festival of color'
+  },
+  {
+    date: '2025-10-15',
+    name: 'Republic Day', 
+    description: 'Independence day'
+  },
+  {
+    date: '2025-10-20',
+    name: 'Diwali', 
+    description: 'The festival of light'
+  },
+];
 
-const FestivalInfo = () => {
+const FestivalCard = (props: FestivalProps) => {
   return (
-    <View style={styles.flexCenter}>
-      <Festival name='Diwali' />
-      <Festival name='Holi' />
-      <Festival name='Republic Day' />
+    <View
+      style={{
+        padding: 20,
+        margin: 100,
+        height: 100,
+        width: 500,
+        borderRadius: 10,
+        alignItems: 'center',
+        alignContent: 'center',
+        backgroundColor: '#2a89d1ff',
+      }}
+    >
+      <Text>Today is {props.name}</Text>
+
     </View>
   );
 };
 
+const HinduCalendar = () => {
+  const [selected, setSelected] = useState('');
+  const [festival, setFestival] = useState('');
+
+  const markedDates = EVENTDATA.reduce((acc, event) => {
+    acc[event.date] = { marked: true, dotColor: '#f3ca13ff', selected: false };
+    return acc;
+  }, {} as { [key: string]: any});
+
+  const combinedMarkedDates = {
+    ...markedDates,
+    [selected]: {selected: true, disableTouchEvent: true, selectedColor: '#f3ca13ff'},
+  };
+
+  const handleDayPress = (day: { dateString: string}) => {
+    // set date as selected
+    setSelected(day.dateString);
+
+    // show event date if date is corresponding to an event
+    const event = EVENTDATA.find((e) => e.date === day.dateString);
+    setFestival(event? event.name : '');
+  };
+
+  const calendarTheme = {
+    backgroundColor: '#ffffff', // Overall background color
+    calendarBackground: '#ffffff', // Calendar background color
+    textSectionTitleColor: '#b6c1cd', // Color for section titles (weekdays)
+    selectedDayBackgroundColor: '#00adf5', // Background color for selected day
+    selectedDayTextColor: '#ffffff', // Text color for selected day
+    todayTextColor: '#f3ca13ff', // Text color for today's date
+    dayTextColor: '#2d4150', // Default day text color
+    textDisabledColor: '#d9e1e8', // Color for disabled days
+    dotColor: '#f3ca13ff', // Default dot color
+    selectedDotColor: '#f3ca13ff', // Dot color for selected day
+    arrowColor: '#f3ca13ff', // Color for navigation arrows
+    monthTextColor: '#f3ca13ff', // Color for month text
+    indicatorColor: 'yellow', // Color for loading indicator
+    textDayFontFamily: 'monospace', // Font family for day numbers
+    textMonthFontFamily: 'monospace', // Font family for month text
+    textDayHeaderFontFamily: 'monospace', // Font family for day headers
+    textDayFontSize: 16, // Font size for day numbers
+    textMonthFontSize: 16, // Font size for month text
+    textDayHeaderFontSize: 16 // Font size for day headers
+  }
+
+  return (
+    <View style={{flexDirection: 'column-reverse', justifyContent: 'space-evenly', backgroundColor: '#2d4150'}}>
+      <Calendar
+        // hideArrows={true}
+        onDayPress={handleDayPress}
+        markedDates={combinedMarkedDates}
+        theme={calendarTheme}
+      />
+
+      {festival && <FestivalCard name={festival}/>}
+    </View>
+  ); 
+};
+
 const App = () => {
   return (
-    <View>
+    <View >
       <HinduCalendar />
-      <FestivalInfo />
     </View>
   );
 }
